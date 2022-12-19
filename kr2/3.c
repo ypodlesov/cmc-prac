09281b4f11c3bdff76695afffde25f9a03f4785e
ypodlesov <ypodlesov@gmail.com>
2022-12-19 11:56:16 +0300

    edit

--- kr2/3.c
+++ kr2/3.c
@@ -7,30 +7,48 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
+
+#define SUCCESS(A) (WIFEXITED(A) && !(WEXITSTATUS(A)))
+
 int
 main(int argc, char *argv[]) 
 {
     setvbuf(stdin, NULL, _IONBF, 0);
     int num;
-    while (scanf("%d", &num) == 1) {
-        printf("%d %d\n", getpid(), num);
+    int flag = 1;
+    scanf("%d", &num);
+    while (1) {
+        if (flag) printf("%d %d\n", getpid(), num);
         fflush(stdout);
+        flag = 0;
         int cur_depth = 0;
+        int fd[2];
+        pipe(fd);
         while (1) {
             if (!fork()) {
+                close(fd[0]);
                 if (++cur_depth == num) {
                     if (scanf("%d", &num) == 1) {
                         printf("%d %d\n", getpid(), num);
                         fflush(stdout);
+                        write(fd[1], &num, sizeof(num));
+                        close(fd[1]);
+                        _exit(0);
+                    } else {
+                        printf("end\n");
+                        close(fd[1]);
+                        _exit(1);
                     }
-                    _exit(0);
                 }
             } else {
-                wait(NULL);
-                if (cur_depth > 0) {
-                    _exit(0);
-                } else {
-                    break;
+                close(fd[1]);
+                int status;
+                wait(&status);
+                if (!cur_depth) {
+                    if (!SUCCESS(status)) {
+                        close(fd[0]);
+                        break;
+                    }
                 }
             }
         }
